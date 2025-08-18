8103ca311199112a6e9ecc118ae3cad35ea95c67
Ngoc-Nhan <nhanvtn7501@ut.edu.vn>
2025-08-18 14:10:36 +0700

    NFF

---
+++ frontend/src/components/NoFriendsFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NoFriendsFound = () => {
+  return (
+    <div className='card bg-base-200 p-5 text-center'>
+      <h3 className='font-semibold text-lg mb-2'>No friends yet</h3>
+      <p className='text-base-content opacity-70'>
+        Connect with languege partners below to start practingcing together
+      </p>
+    </div>
+  )
+}
+
+export default NoFriendsFound
